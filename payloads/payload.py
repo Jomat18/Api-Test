08e9a816410a843e5bde3d89e9b968c3f3f6a00c
Jomat18 <manu.jose.torres@gmail.com>
2021-03-10 23:39:40 -0500

    feat: post tercer producto

---
+++ payloads/payload.py
@@ -0,0 +1,108 @@
+payload = {
+    "color": "negro",
+    "price": 99,
+    "sku": "L4E7S066770",
+    "stock": 30,
+    "condition": "new",
+    "size": "22.00",
+    "part_number": "47200-220",
+    "listing_type": "gold_special",
+    "images": [
+      {
+        "url": "https://s.fenicio.app/f/menpuy/catalogo/articulos/SC3604309-0-1_460x460_1578160987_059.jpg"
+      }
+    ],
+    "name": "Ball",
+    "brand": "Ball",
+    "depth": 21,
+    "description": "Plataforma Abierta Para Mujer Rafael Ferrigno 2704-047200 Color Negro En Acabado Sillero",
+    "dimensions_unit": "cm",
+    "height": 11,
+    "gender": "female",
+    "shipping_depth": 21,
+    "shipping_height": 11,
+    "shipping_price": 0,
+    "shipping_width": 31,
+    "shipping": 0,
+    "sku_simple": "L4E7S066770",
+    "weight": 0.8,
+    "weight_unit": "kg",
+    "tax": "16",
+    "width": 31,
+    "variations": [
+      {
+        "color": "negro",
+        "price": 699,
+        "sku": "L4E7S066775",
+        "stock": 0,
+        "size": "22.50",
+        "part_number": "47200-225",
+        "images": [
+          {
+            "url": "https://i.pinimg.com/564x/69/45/27/6945276147be0e46ec0e840adf34e59a.jpg"
+          }
+        ],
+        "product_weight": "0.8",
+      },
+      {
+        "color": "negro",
+        "price": 699,
+        "sku": "L4E7S066780",
+        "stock": 0,
+        "size": "23.00",
+        "part_number": "47200-230",
+        "images": [
+          {
+            "url": "https://www.regalopublicitario.net/wp-content/uploads/2018/07/antiestres-pelota-colores-espuma.jpg"
+          }
+        ],
+        "product_weight": "0.8",
+      },
+      {
+        "color": "negro",
+        "price": 699,
+        "sku": "L4E7S066785",
+        "stock": 0,
+        "size": "23.50",
+        "part_number": "47200-235",
+        "images": [
+          {
+            "url": "https://www.pepeganga.com.uy/content/images/thumbs/0054823_35fc8f3d040ee2bc847a96ce5c8458bb3b6ba92372afb84cafdf37ef0a458fae_600.jpeg"
+          }
+        ],
+        "product_weight": "0.8",
+      },
+      {
+        "color": "negro",
+        "price": 699,
+        "sku": "L4E7S066790",
+        "stock": 0,
+        "size": "24.00",
+        "part_number": "47200-240",
+        "images": [
+          {
+            "url": "https://st.depositphotos.com/1454700/3084/i/600/depositphotos_30842905-stock-photo-brazilian-soccer-ball.jpg"
+          }
+        ],
+        "product_weight": "0.8",
+      },
+      {
+        "color": "negro",
+        "price": 699,
+        "sku": "L4E7S066795",
+        "stock": 0,
+        "size": "24.50",
+        "part_number": "47200-245",
+        "images": [
+          {
+            "url": "https://www.regalopublicidad.com/images/1hy14/a612a3cbe1e4805d4cbd5831f9a2/610-460/pelota-de-playa-colores-varios-azul.jpg"
+          }
+        ],
+        "product_weight": "0.8",
+      }
+    ],
+    "description_html": "<strong>Plataforma para Mujer Rafael Ferrigno 2704-047200 Color Negro</strong>",
+    "template_html": "default_plain_text",
+    "model": "47200-220-047200",
+    "category_pk": 559
+}
